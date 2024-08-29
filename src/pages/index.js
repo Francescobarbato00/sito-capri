f9ea7283864fbf002bc5834dfb8d10d327ec88f3
Francescobarbato00 <francesco.barbato24@gmail.com>
2024-08-29 22:50:28 +0200

    prova

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,10 @@ import Header from './components/Header';
 import ScrollingLogos from './components/ScrollingLogos';  // Import the ScrollingLogos component
 import LogisticsInfoSection from './components/LogisticsInfoSection'; // Import the new section
 import Footer from './components/Footer'; // Import the footer
-import { CommentSuggestionSection, VideoSection } from './components/CommentVideoSections';
-import { ContactForm, GoogleMap } from './components/ContactMapSections';
+import CommentSuggestionSection from './components/CommentSuggestionSection,';
+import VideoSection from './components/VideoSection,';
+import ContactForm from './components/ContactForm, ';
+import GoogleMap from './components/GoogleMap';
 import ChatButton from "./components/ChatButton";
 export default function Home() {
   return (
